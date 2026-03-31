c4cd9a8b4293700cdf0afdc6faaa2fe6faafc4a8
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-30 19:37:19 -0600

    html and com protocol was set

--- api.py
+++ api.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "hola mundo dese fast api que no  compila pero si compila"}
+IMAGE_PATH = "images/latest.jpg"
+
+@app.get("/latest")
+def get_latest():
+    return FileResponse(IMAGE_PATH, media_type="image/jpeg")
+
+
+@app.post("/upload")
+async def upload_image(request: Request):
+    data = await request.body()
+
+    with open("images/latest.jpg", "wb") as f:
+        f.write(data)
+
+    return {"status": "saved"}
