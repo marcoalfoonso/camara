4b76e4de4f280c23de4d406986bb1c5cbc49f188
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-04-02 14:02:52 -0600

    camara en ip dir

--- api.py
+++ api.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 import os
 
 app = FastAPI()
 
 IMAGE_PATH = "images/latest.jpg"
 
-@app.get("/latest")
-def get_latest():
-    return FileResponse(IMAGE_PATH, media_type="image/jpeg")
-
-
 @app.post("/upload")
 async def upload_image(request: Request):
     data = await request.body()
+
+    os.makedirs("images", exist_ok=True)
 
     with open("images/latest.jpg", "wb") as f:
         f.write(data)
 
     return {"status": "saved"}
+
+
+@app.get("/latest")
+def get_latest():
+    if not os.path.exists("images/latest.jpg"):
+        return JSONResponse({"error": "No image yet"}, status_code=404)
+    
+    return FileResponse(IMAGE_PATH, media_type="image/jpeg")
